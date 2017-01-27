6d7bb73e52c1264a9e5f8aaa466429ec47c03f37
mad0 <madowaty@gmail.com>
2017-01-27 14:18:56 +0100

    New data in data2.ort

--- ConsoleRPG/main.cpp
+++ ConsoleRPG/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include "JsonBox.h"
 #include <string>
+#include <map>
 
 int main() {
+	std::map <std::string, std::string> data;
 	JsonBox::Value f;
 	f.loadFromFile("data2.ort");
 	JsonBox::Object o = f.getObject();
 	
 	for (auto i : o) {
-		std::string w = i.first;
-		std::cout << w << "\n";
+		std::string z = i.first;
+		auto w = i.second;
+
+		std::cout <<w;
 	}
 		
 
