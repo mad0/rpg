21cc9068fdb87430f5c7e5efb662d7baab585248
mad <mad@DESKTOP-P27D63S>
2017-01-27 06:37:24 +0100

    JsonBox, fisrt,second....

--- ConsoleRPG/main.cpp
+++ ConsoleRPG/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "JsonBox.h"
+#include <string>
 
 int main() {
 	JsonBox::Value f;
+	f.loadFromFile("data.ort");
+	JsonBox::Object o = f.getObject();
+	
+	for (auto i : o) {
+		std::string w = i.second;
+		std::cout << w << "\n";
+	}
+		
 
 	return 0;
 };
