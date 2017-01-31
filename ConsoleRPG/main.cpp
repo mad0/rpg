0280353ead1a7946d0f252dde6be08db7b887d87
mad <mad@DESKTOP-P27D63S>
2017-01-31 22:08:54 +0100

    std::stack: OK

--- ConsoleRPG/main.cpp
+++ ConsoleRPG/main.cpp
@@ -8,6 +8,10 @@
 
 class Baza {
 public:
+	Baza() {
+		//++ile;
+		//std::cout << "POCHODNA: " << ile << "\n";
+	}
 	virtual void fun() {
 		std::cout << "BAZOWA\n";
 	}
@@ -15,9 +19,16 @@ public:
 };
 
 class Pochodna : public Baza {
+private:
+	std::string t;
 public:
+	static int ile;
+	Pochodna(const std::string &z) : t(z) {
+		++ile;
+	}
+	
 	void fun() {
-		std::cout << "POCHODNA\n";
+		std::cout << "POCHODNA: Obiekt nr: " << t << "\n";
 	}
 
 };
@@ -25,13 +36,25 @@ public:
 std::stack <Baza*> dane;
 
 Baza* klasa() {
+	std::cout << "Ilosc obiektow na stosie: " << dane.size() << "\n";
 	return dane.top();
 }
+
+int Pochodna::ile = 0;
 
 int main() {
 	
 	Baza **wsk = new Baza* [4];
-	//dane.push(wsk);
+	//Baza *wsk = new Pochodna();
+	wsk[0] = new Pochodna("a");
+	wsk[1] = new Pochodna("b");
+	wsk[2] = new Pochodna("c");
+	wsk[3] = new Pochodna("d");
+	dane.push(wsk[0]);
+	dane.push(wsk[1]);
+	dane.push(wsk[2]);
+	dane.push(wsk[3]);
+	dane.pop();
 	/*std::map <std::string, JsonBox::Value> data;
 	JsonBox::Value f;
 	f.loadFromFile("data2.ort");
@@ -49,6 +72,13 @@ int main() {
 	//wsk->fun();
 	//klasa()->fun();
 	//std::cout << dane.size() << "\n";
+	//wsk->fun();
+	/*wsk[0]->fun();
+	wsk[1]->fun();
+	wsk[2]->fun();
+	wsk[3]->fun();
+	*/
+	klasa()->fun();
 	return 0;
 };
 
